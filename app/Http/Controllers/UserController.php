38b844f2de4e6d871f7b90c81c48be24c38ef6ef
= <belqisivana7@gmail.com>
2025-05-10 10:59:07 +0700

    prak2js4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,23 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\UserModel;
 use Illuminate\Http\Request;
+use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
-
 
 class UserController extends Controller
 {
     public function index()
-    {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
+{
+    $data = [
+        'level_id' => 2,
+        'username' => 'manager_tiga',
+        'nama' => 'Manager 3',
+        'password' => Hash::make('12345')
+    ];
+    
+    $user = UserModel::findOr(20, ['username', 'nama'], function () {
+    abort(404);
+    });
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+    return view('user', ['data' => $user]);
+    
     }
 }
